a30aa43e4223cb15e389f41037701c6dd62f02e8
PandyYang <yangpandy@gmail.com>
2021-07-31 13:18:36 +0800

    接口中可以有默认的方法实现

---
+++ src/main/java/com/pandy/base/java/java8/A.java
@@ -0,0 +1,11 @@
+package com.pandy.base.java.java8;
+
+/**
+ * @Author Pandy
+ * @Date 2021/7/31 12:35
+ */
+public interface A{
+    default void foo() {
+        System.out.println("默认方法实现，加上关键字default");
+    }
+}
---
+++ src/main/java/com/pandy/base/java/java8/Clazz.java
@@ -0,0 +1,12 @@
+package com.pandy.base.java.java8;
+
+/**
+ * @Author Pandy
+ * @Date 2021/7/31 12:36
+ */
+public class Clazz implements A {
+    public static void main(String[] args) {
+        Clazz clazz = new Clazz();
+        clazz.foo();
+    }
+}
