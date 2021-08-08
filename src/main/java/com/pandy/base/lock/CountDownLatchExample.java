1796c81cc7ba6b788fa39e4d410667ae64031d00
PandyYang <yangpandy@gmail.com>
2021-08-08 21:27:06 +0800

    CountDownlatch 和 CyclicBarrier示例

--- src/main/java/com/pandy/base/lock/CountDownLatchExample.java
+++ src/main/java/com/pandy/base/lock/CountDownLatchExample.java
@@ -9,18 +9,53 @@ import java.util.concurrent.Executors;
  * @Date 2021/7/20 22:19
  */
 public class CountDownLatchExample {
-    public static void main(String[] args) throws InterruptedException {
-        final int totalThread = 20;
-        CountDownLatch count = new CountDownLatch(totalThread);
-        ExecutorService executorService = Executors.newCachedThreadPool();
-        for (int i = 0; i < totalThread; i++) {
-            executorService.execute(() -> {
-                System.out.println("run...");
-                count.countDown();
-            });
+//    public static void main(String[] args) throws InterruptedException {
+//        final int totalThread = 20;
+//        CountDownLatch count = new CountDownLatch(totalThread);
+//        ExecutorService executorService = Executors.newCachedThreadPool();
+//        for (int i = 0; i < totalThread; i++) {
+//            executorService.execute(() -> {
+//                System.out.println("run...");
+//                count.countDown();
+//            });
+//        }
+//        count.await();
+//        System.out.println("end");
+//        executorService.shutdown();
+//    }
+//
+    private static int LATCH_SIZE = 5;
+    private static CountDownLatch doneSingle;
+
+    public static void main(String[] args) {
+        try {
+            doneSingle = new CountDownLatch(LATCH_SIZE);
+
+            // 创建5个任务
+            for (int i = 0; i < LATCH_SIZE; i++) {
+                new InnerThread().start();
+            }
+
+            System.out.println("main await begin.");
+            // 主线程等待线程池中的5个任务完成
+            doneSingle.await();
+            System.out.println("main thread finished");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        count.await();
-        System.out.println("end");
-        executorService.shutdown();
+    }
+
+    static class InnerThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + " sleep 1000 ms");
+                // 将countDownLatch的数值减1
+                doneSingle.countDown();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/com/pandy/base/lock/CyclicBarrierExample.java
+++ src/main/java/com/pandy/base/lock/CyclicBarrierExample.java
@@ -10,23 +10,53 @@ import java.util.concurrent.Executors;
  * @Date 2021/7/20 22:25
  */
 public class CyclicBarrierExample {
+//    public static void main(String[] args) {
+//        final int totalThread = 20;
+//        CyclicBarrier cyclicBarrier = new CyclicBarrier(totalThread);
+//        ExecutorService executorService = Executors.newCachedThreadPool();
+//        for (int i = 0; i < totalThread; i++) {
+//            executorService.execute(() -> {
+//                System.out.println("before");
+//                try {
+//                    cyclicBarrier.await();
+//                } catch (BrokenBarrierException e) {
+//                    e.printStackTrace();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println("after");
+//            });
+//        }
+//        executorService.shutdown();
+//    }
+
+    /**
+     *  创建了5个线程 所有这些线程都是调用cb.await等待 所有这些线程一直等待
+     *  直到cb中所所有线程都达到barrier时，这些线程才继续允许。
+     */
+    private static int SIZE = 5;
+    private static CyclicBarrier cb;
+
     public static void main(String[] args) {
-        final int totalThread = 20;
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(totalThread);
-        ExecutorService executorService = Executors.newCachedThreadPool();
-        for (int i = 0; i < totalThread; i++) {
-            executorService.execute(() -> {
-                System.out.println("before");
-                try {
-                    cyclicBarrier.await();
-                } catch (BrokenBarrierException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("after");
-            });
+        cb = new CyclicBarrier(SIZE);
+        for (int i = 0; i < SIZE; i++) {
+            new InnerThread().start();
         }
-        executorService.shutdown();
+    }
+
+
+    static class InnerThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                System.out.println(Thread.currentThread().getName() + " wait for CyclicBarrier");
+                cb.await();
+                System.out.println(Thread.currentThread().getName() + " continued");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
