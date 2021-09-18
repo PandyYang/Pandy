9ef1f7221657bc992bddd08381bf0594756158bc
PandyYang <yangpandy@gmail.com>
2021-09-18 10:27:55 +0800

    rocket mq test，vue事件修饰符测试

--- src/main/java/com/pandy/go/pointor.go
+++ src/main/java/com/pandy/go/pointor.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 
-	var a = 20  // 声明实际变量
-	var ip *int // 声明指针变量
+	var a = 20 // 声明实际变量
+	var _ *int // 声明指针变量
 
-	ip = &a // 指针变量的存储地址
+	_ = &a // 指针变量的存储地址
 	fmt.Printf("a 变量的地址是")
 }
