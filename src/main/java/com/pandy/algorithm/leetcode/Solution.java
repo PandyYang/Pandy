02da1598ef0c0ad2d28116af8e62e580e03eb683
Pandy <yangpandy@gmail.com>
2021-09-25 20:55:24 +0800

    合并两个数组 保持有序

--- src/main/java/com/pandy/algorithm/leetcode/Solution.java
+++ src/main/java/com/pandy/algorithm/leetcode/Solution.java
@@ -1,5 +1,6 @@
 package com.pandy.algorithm.leetcode;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -23,5 +24,37 @@ class Solution {
             }
         }
         return res;
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+//        int j = 0;
+//        for (int i = nums1.length-m; i <= m+n && nums2.length > j; i++) {
+//            nums1[i] = nums2[j];
+//            j++;
+//        }
+//        Arrays.sort(nums1);
+//        for (int i : nums1) {
+//            System.out.println("i = " + i);
+//        }
+    }
+
+    public static void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int tail = m + n - 1;
+        while (p2 >= 0) {
+            nums1[tail--] = (p1 >= 0 && nums1[p1] > nums2[p2]) ? nums1[p1--] : nums2[p2--];
+        }
+        for (int i : nums1) {
+            System.out.println(i);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+//        merge(new int[]{1,2,3,0,0,0}, 3, new int[]{2,5,6}, 3);
+//        merge(new int[]{0}, 0, new int[]{1}, 1);
+        merge2(new int[]{0}, 0, new int[]{1}, 1);
     }
 }
