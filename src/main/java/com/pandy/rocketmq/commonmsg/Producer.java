927f95dc56ef26eeed04be0ea521043a7e1774a1
Pandy <yangpandy@gmail.com>
2021-11-13 09:07:11 +0800

    rocketmq生产者消费者

--- src/main/java/com/pandy/rocketmq/commonmsg/Producer.java
+++ src/main/java/com/pandy/rocketmq/commonmsg/Producer.java
@@ -1,28 +1,47 @@
 package com.pandy.rocketmq.commonmsg;
 
-import org.apache.rocketmq.client.exception.MQBrokerException;
-import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
-import org.apache.rocketmq.remoting.exception.RemotingException;
 
-import java.io.UnsupportedEncodingException;
+import java.util.concurrent.Semaphore;
 
-/**
- * @Author Pandy
- * @Date 2021/11/11 22:20
- */
 public class Producer {
-    public static void main(String[] args) throws MQClientException, UnsupportedEncodingException, MQBrokerException, RemotingException, InterruptedException {
-        DefaultMQProducer producer = new DefaultMQProducer("ProducerGroup");
-        producer.setNamesrvAddr("47.95.216.30:9876");
-        producer.start();
-        Message msg = new Message("TopicTest",
-                "HelloWorld".getBytes(RemotingHelper.DEFAULT_CHARSET));
-        SendResult sendResult = producer.send(msg);
-        System.out.println("sendResult = " + sendResult);
-        producer.shutdown();
-    }
+
+	private static int count = 100;
+
+	public static void main(String[] args) throws Exception {
+		// 设置生产者组名
+		DefaultMQProducer producer = new DefaultMQProducer("ProducerGroupName");
+		// 指定nameServer的地址
+		producer.setNamesrvAddr("192.168.218.129:9876;192.168.218.130:9876");
+		// 启动实例
+		producer.start();
+
+		final Semaphore semaphore = new Semaphore(0);
+
+		for (int i = 0; i < count; i++) {
+			Thread.sleep(3000);
+			Message message = new Message("TopicTest",
+					"test_tag",
+					("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+
+			producer.send(message, new SendCallback() {
+				public void onSuccess(SendResult sendResult) {
+					System.out.println(String.format("message [%s] send success!", new String(message.getBody())));
+					semaphore.release();
+				}
+
+				public void onException(Throwable throwable) {
+					throwable.printStackTrace();
+				}
+			});
+
+		}
+		semaphore.acquire(count);
+		//关闭生产者，释放资源
+		producer.shutdown();
+	}
 }
