b63fb49a0da652ed2773d3fc1047d28c77b33cff
PandyYang <yangpandy@gmail.com>
2021-11-15 12:04:13 +0800

    处理发送请求每次只发送500条

--- src/main/java/com/pandy/go/main.go
+++ src/main/java/com/pandy/go/main.go
@@ -4,6 +4,17 @@ import "strings"
 
 func main() {
 
+	arr := [1000]int{1, 2, 3}
+	for idx := 0; idx < len(arr); idx += 500 {
+		end := len(arr)
+		if idx+500 < end {
+			end = idx + 499
+		}
+
+		s := arr[idx:end]
+		println(len(s))
+	}
+
 	s := "123.321"
 	println(len(s))
 	ss := s[3:len(s)]
