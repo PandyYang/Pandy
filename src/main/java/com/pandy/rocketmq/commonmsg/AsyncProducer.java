79f768efd5a9cee8a948ef5415afca7df35d9c0b
PandyYang <yangpandy@gmail.com>
2021-11-16 10:02:53 +0800

    异步发送消息

---
+++ src/main/java/com/pandy/rocketmq/commonmsg/AsyncProducer.java
@@ -0,0 +1,43 @@
+package com.pandy.rocketmq.commonmsg;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CountDownLatch;
+
+public class AsyncProducer {
+    public static void main(String[] args) throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        producer.setNamesrvAddr("47.95.216.30:9876");
+        producer.start();
+        producer.setRetryTimesWhenSendAsyncFailed(0);
+        int messageCount = 100;
+        final CountDownLatch countDownLatch = new CountDownLatch(messageCount);
+        for (int i = 0; i < messageCount; i++) {
+            try {
+                final int index = i;
+                Message msg = new Message("Jodie_topic_1023", "TagA", "OrderID188"
+                , "Hello World".getBytes(StandardCharsets.UTF_8));
+                producer.send(msg, new SendCallback() {
+                    @Override
+                    public void onSuccess(SendResult sendResult) {
+                        countDownLatch.countDown();
+                        System.out.printf("%-10d OK %s %n", index, sendResult.getMsgId());
+                    }
+
+                    @Override
+                    public void onException(Throwable throwable) {
+                        countDownLatch.countDown();
+                        System.out.printf("%-10d Exception %s %n", index, throwable);
+                        throwable.printStackTrace();
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
