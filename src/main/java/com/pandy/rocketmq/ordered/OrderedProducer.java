e9ddced803256eba48a4f787c66374866faac8f6
PandyYang <yangpandy@gmail.com>
2021-11-16 11:40:56 +0800

    顺序消息

---
+++ src/main/java/com/pandy/rocketmq/ordered/OrderedProducer.java
@@ -0,0 +1,37 @@
+package com.pandy.rocketmq.ordered;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+/**
+ * 使用FIFO队列对消息进行排序
+ */
+public class OrderedProducer {
+    public static void main(String[] args) throws Exception {
+        MQProducer producer = new DefaultMQProducer("example_group_name");
+        producer.start();
+        String[] tags = new String[] {"TagA", "TagB", "TagC", "TagD", "TagE"};
+        for (int i = 0; i < 100; i++) {
+            int orderId = i % 10;
+            Message msg = new Message("TopicTestjjjj", tags[i % tags.length], "KEY" + i,
+                    ("Hello RocketMQ" + i).getBytes(StandardCharsets.UTF_8));
+            SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
+                @Override
+                public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                    Integer id = (Integer) arg;
+                    int index = id % mqs.size();
+                    return mqs.get(index);
+                }
+            }, orderId);
+            System.out.printf("%s%n", sendResult);
+        }
+        producer.shutdown();
+    }
+}
