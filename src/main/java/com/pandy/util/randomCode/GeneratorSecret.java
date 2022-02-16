af4a412acd9f777483bfd4f366ea22ae3bfb994b
PandyYang <yangpandy@gmail.com>
2022-02-16 16:57:37 +0800

    get random code

--- src/main/java/com/pandy/util/randomCode/GeneratorSecret.java
+++ src/main/java/com/pandy/util/randomCode/GeneratorSecret.java
@@ -1,4 +1,4 @@
-package com.pandy.secret;
+package com.pandy.util.randomCode;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
@@ -6,5 +6,9 @@ public class GeneratorSecret {
     public static void main(String[] args) {
         String s = RandomStringUtils.randomAlphanumeric(20);
         System.out.println(s);
+
+
     }
+
+
 }
