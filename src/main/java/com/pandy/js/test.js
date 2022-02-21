fabbd55bc10994e2c61fb131f4fb2476142f2ce0
PandyYang <yangpandy@gmail.com>
2022-02-21 16:14:15 +0800

    复制换行符，复制单行文本

---
+++ src/main/java/com/pandy/js/test.js
@@ -0,0 +1,4 @@
+var res = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n" +
+    "<p>你好，请先登录下载中心，再输入以下地址</p><br>\n" +
+    "<p>地址：</p>" + "<p>" + 123 + "</p>>"
+
---
+++ src/main/java/com/pandy/vue/test.js
@@ -0,0 +1,21 @@
+
+
+function handleCopyWithAuth() {
+    var text = 123
+
+    let copyHandler = copy(text)
+
+    document.addEventListener('copy', copyHandler)
+    document.execCommand('copy')
+    removeEventListener('copy', copyHandler)
+}
+
+function copy(article) {
+    return function (event) {
+        event.clipboardData.setData("text/html", article)
+        event.preventDefault()
+    }
+}
+
+
+handleCopyWithAuth()
